15d0b9e2ffe971cc16caa367bf50cd782dc85d6e
jjad85 <jjad85@gmail.com>
2020-02-16 12:04:55 -0500

    controller correction

--- secondApp/app/controllers/propController.js
+++ secondApp/app/controllers/propController.js
@@ -11,7 +11,7 @@ exports.obtenerpropietarios = async (req, res) => {
 
 exports.crearpropietario = async (req, res) => {
   try {
-    Propietario = await aptoService.crearpropietario(req.body);
+    Propietario = await propService.crearpropietario(req.body);
     res.status(200).send(Propietario);
   }catch(err){
     res.status(500).send(err);
@@ -20,7 +20,7 @@ exports.crearpropietario = async (req, res) => {
 
 exports.elimnarpropietarios = async (req, res) => {
   try {
-    Propietario = await aptoService.elimnarpropietarios(req.params.id);
+    Propietario = await propService.elimnarpropietarios(req.params.id);
     res.status(200).send(Propietario);
   }catch(err){
     res.status(500).send(err);
@@ -29,7 +29,7 @@ exports.elimnarpropietarios = async (req, res) => {
 
 exports.findpropietario = async (req, res) => {
   try {
-    Propietario = await aptoService.findpropietario(req.params.id);
+    Propietario = await propService.findpropietario(req.params.id);
     res.status(200).send(Propietario);
   }catch(err){
     res.status(500).send(err);
@@ -38,7 +38,7 @@ exports.findpropietario = async (req, res) => {
 
 exports.updatepropietario = async (req, res) => {
   try {
-    Propietario = await aptoService.updatepropietario(req.params.id, req.body);
+    Propietario = await propService.updatepropietario(req.params.id, req.body);
     res.status(200).send(Propietario);
   }catch(err){
     res.status(500).send(err);
